799117e9cc721803cf9cde50fb36b4f5dd8a90af
SeaBassy4 <sebastianperezgonzalez4@gmail.com>
2025-08-20 15:43:44 -0700

    27 - Bucket List

---
+++ 27 - Bucket List.py
@@ -0,0 +1,15 @@
+things_to_do = [
+  'Make at least one game that makes it to GOTY.',
+  'Start and finish my Master''s degree in Germany.',
+  'Get ripped',
+  'Stay ripped all my life',
+  'Keep an aquarium in my house',
+  'Have some cats and dogs when I live alone.',
+  'Live a quiet but fun life.',
+  'Get married and start a family.',
+  'Ser un pinche ganador que escucha PP'
+]
+
+
+for i in range(len(things_to_do)):
+  print(things_to_do[i])
